c0004214a54a016a4a03ad4085a9be614b9b183e
Arthur Cassim <arthurcassim.dev@gmail.com>
2026-05-05 18:30:39 -0300

    Create ex2.py

---
+++ aula-07/ex2.py
@@ -0,0 +1,11 @@
+n = int(input("Digite um número: "))
+contador = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        contador += 1
+
+if contador == 2:
+    print("Primo")
+else:
+    print("Não primo")
